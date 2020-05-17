df5ff327a01cf4f551268776c772f38a5f09f921
Ehsan Asdar <ehsanmasdar@gmail.com>
2020-05-17 14:05:36 -0500

    Fix 404

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
     resources :github_webhooks, only: [:create], defaults: { formats: :json }
     resources :version_updates, only: [:create], defaults: { formats: :json }
-  end  
+  end
+  match '*any', to: 'errors#not_found', via: [:get, :post]
 end
